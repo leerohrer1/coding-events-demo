2fadbbbbefb75ddfe575ec967a753980de00c259
leerohrer1 <loginlee3@gmail.com>
2022-11-09 19:22:16 -0600

    Finished studio

---
+++ src/main/java/org/launchcode/codingevents/models/AbstractEntity.java
@@ -0,0 +1,31 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int Id;
+
+    public int getId() {
+        return Id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return Id == that.Id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id);
+    }
+}
